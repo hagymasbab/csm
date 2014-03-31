d100def2ae1548ffdf28d74d331e0a4522a6235e
Mihály Bányai <hagymasbab@gmail.com>
2014-03-31 19:58:32 +0200

    three-quarters-done batch support

--- matlab/gestaltPostVRnd.m
+++ matlab/gestaltPostVRnd.m
@@ -1,12 +1,20 @@
-function v = gestaltPostVRnd(ge,xind,g)
+function V = gestaltPostVRnd(ge,xind,g)
     % TODO support batches
     % construct the covariance and mean of the conditional posterior over v
     sAA = (1/ge.obsVar) * ge.AA;
-    Ax = ge.tX(xind,:)';
     iCv = inv(componentSum(g,ge.cc));
     cov = inv(sAA + iCv);
-    % TEST: kivettem a minuszt
-    m = (2/ge.obsVar) * cov * Ax;
+    largecov = [];
+    largemean = zeros(ge.B*ge.Dv,1);
+    for b=1:ge.B
+        largecov = blkdiag(largecov,cov);
+        Ax = reshape(ge.tX(xind,b,:),1,ge.Dv)';
+        % TEST: kivettem a minuszt
+        m = (2/ge.obsVar) * cov * Ax;
+        largemean(1+(b-1)*ge.Dv:b*ge.Dv,1) = m;
+    end
+    
     % generate a sample from this distribution
-    v = mvnrnd(m',cov)';
+    V = mvnrnd(largemean',largecov);
+    V = reshape(V,ge.B,ge.Dv);
 end
