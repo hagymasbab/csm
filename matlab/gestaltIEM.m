a3f5771a59876ef1834f36fef350d9c6e29db73b
Mihály Bányai <hagymasbab@gmail.com>
2014-05-07 16:32:29 +0200

    organizing IEM

--- matlab/gestaltIEM.m
+++ matlab/gestaltIEM.m
@@ -16,6 +16,15 @@ function [diff,longdiff] = gestaltIEM(ge,X,nSamples,maxStep,randseed,varargin)
     verb = parser.Results.verbose;
     multistep = parser.Results.multistep;
     
+    if plot>1
+        subplot = @(m,n,p) subtightplot (m, n, p, [0.025 0.001], [0 0.025], [0 0.01]);
+        clf;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+    % SET RANDOM SEED          
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
     if strcmp(randseed,'last')
         load lastrandseed;
     end
@@ -23,19 +32,28 @@ function [diff,longdiff] = gestaltIEM(ge,X,nSamples,maxStep,randseed,varargin)
     RandStream.setGlobalStream(s);
     randseed = s.Seed;
     save('lastrandseed.mat','randseed');
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+    % CREATE INITAL PARAMETER MATRICES AND MODEL STRUCTURE     
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
-    ccInit = randomCovariances(ge.k,ge.Dv,'precision',precision);
+    ccInit = randomCovariances(ge.k,ge.Dv,'precision',precision);        
+    cholesky = cellchol(ccInit);   
+    pCC{1} = ccInit;            
     
-    X_old = ge.X;
-    N_old = ge.N;
+    cc_old = extractComponents(ge,precision);    
+    ge = replaceComponents(ge,ccInit,precision);
+    ge.X = X;
+    ge.N = size(ge.X,1);
     
-    if ~precision
-        cc_old = ge.cc;
-        ge.cc = ccInit;
-    else
-        cc_old = ge.pc;
-        ge.pc = ccInit;
-    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+    % INITIALISE ARRAYS FOR SAVING VALUES   
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
+        
+    % maximum change of a parameter over a cycle should not be more than:
+    goaldiff = (1 / ge.N) * ones(ge.Dv);
+    % empirical correction of the dimension dependence of the largest eigenvalue of the inverse covariance
+    goaldiff = goaldiff / (ge.Dv * 0.025);
     
     minperm = [];
     diff = zeros(1,maxStep+1);
@@ -43,30 +61,15 @@ function [diff,longdiff] = gestaltIEM(ge,X,nSamples,maxStep,randseed,varargin)
     if nargout > 1
         longdiff = zeros(1,maxStep*ge.N+1);
         longdiff(1,1) = diff(1,1);
-    end    
+    end              
     
-    ge.X = X;
-    ge.N = size(ge.X,1);
-    sdim = ge.k+(ge.Dv*ge.B);
-    % maximum change of a parameter over a cycle should not be more than:
-    goaldiff = (1 / ge.N) * ones(ge.Dv);
-    % empirical correction of the dimension dependence of the largest eigenvalue of the inverse covariance
-    goaldiff = goaldiff / (ge.Dv * 0.025);
-    
-    cholesky = cellchol(ccInit);  
-    cholparnum = (ge.Dv^2 + ge.Dv) / 2;
-    
-    pCC{1} = ccInit;
     S = {};
-    
-    if plot>1
-        subplot = @(m,n,p) subtightplot (m, n, p, [0.025 0.001], [0 0.025], [0 0.01]);
-        clf;
-    end
-    
-    cc_next = cell(1,ge.k);
+    sdim = ge.k+(ge.Dv*ge.B);
     samples = zeros(ge.N,nSamples,sdim);
     mean_gradient = zeros(1,maxStep*ge.N);
+    cdll_array = zeros(1,maxStep*ge.N);
+    cc_next = cell(1,ge.k);
+    
     for i=1:maxStep
         if verb > 1
             fprintf('IEM cycle %d datapoint %d/',i,ge.N);
@@ -74,29 +77,26 @@ function [diff,longdiff] = gestaltIEM(ge,X,nSamples,maxStep,randseed,varargin)
         if plot>1
             nopause = false;
         end
-        if ~precision
-            cc_prev = ge.cc;
-        else
-            cc_prev = ge.pc;
-        end
         
+        cc_prev = extractComponents(ge,precision);
         skipped = 0;
         avgrate = 0;
+        
         for n=1:ge.N
+            lidx = 1+(i-1)*ge.N+n;
             if verb>1
                 printCounter(n);
-            end
-            if verb > 1
                 fprintf(' ');
             end
             
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
-            % ACTUAL IEM PART BEGINS HERE            
+            % E - step            
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
-            % E-step: Gibbs sampling
+            % Gibbs sampling
             [samples(n,:,:),rr] = gestaltGibbs(ge,n,nSamples,'verbose',verb-1,'precision',precision,'approximatePostCov',approx);            
-            if rr < 0                
+            % if couldn't find a valid g-sample in 10 steps, skip
+            if rr < 0                %%%%
                 if verb>1
                     fprintf('\b');                
                 end
@@ -104,54 +104,58 @@ function [diff,longdiff] = gestaltIEM(ge,X,nSamples,maxStep,randseed,varargin)
                 continue;
             end
             
-            % M-step: gradient ascent            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+            % M - step            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % gradient of the parameters of the complete-data log-likelihood            
             grad = gestaltParamGrad(ge,samples(n,:,:),cholesky,'precision',precision);                        
             
-            % choose learning rate
-            meanvals = zeros(1,ge.k);
-            for j=1:ge.k
-                meanvals(1,j) = meanvals(1,j) + mean(mean(abs(grad{j}),2),1);
-            end
-            meanval = mean(meanvals,2);  
-            mean_gradient(1,(i-1)*ge.N+n) = meanval;
+            % mean of gradient for learning rate calculation
+            [gradAvg,gradCompAvgs] = componentAbsMeans(grad);
+            mean_gradient(1,(i-1)*ge.N+n) = gradAvg;
             
             % TEST: skip large gradients
-            if meanval > 5
+            if gradAvg > 5
                 skipped = skipped + 1;
                 if verb>1
-                    for b=1:9+2*(floor(log10(nSamples))+1)
-                        fprintf('\b');
-                    end
+                    delPrint(nSamples);
+                end
+                if nargout > 1                    
+                    longdiff(1,lidx) = longdiff(1,lidx-1);
                 end
                 continue;
             end
-            
-            chol_cand = cholesky;
-            cdll = gestaltCompleteDataLogLikelihood(ge,samples(n,:,:),cholesky);
+                        
             actrate = cell(1,ge.k);
             avgrates = zeros(1,ge.k);
             for j=1:ge.k
-                % choose learning rate
+                % choose learning rate according to selected method
                 if strcmp(ratemethod,'elementwise_goal')
                     actrate = min(goaldiff ./ abs(grad{j}),lrate * ones(ge.Dv));
                 elseif strcmp(ratemethod,'mean_goal')
-                    actrate = min(goaldiff / meanval,lrate * ones(ge.Dv));
+                    actrate = min(goaldiff / gradAvg,lrate * ones(ge.Dv));
                 elseif strcmp(ratemethod,'componentwise_goal')                  
-                    actrate{j} = min(goaldiff / meanvals(1,j),lrate * ones(ge.Dv));
+                    actrate{j} = min(goaldiff / gradCompAvgs(1,j),lrate * ones(ge.Dv));
                 elseif strcmp(ratemethod,'only_goal')
-                    actrate{j} = goaldiff / meanvals(1,j);
+                    actrate{j} = goaldiff / gradCompAvgs(1,j);
                 elseif strcmp(ratemethod,'only_rate')
                     actrate{j} = lrate * ones(ge.Dv);
                 else
                     exit('Invalid learning rate method: %s',ratemethod);
                 end
-                avgrates(1,j) = sum(sum(actrate{j}))/cholparnum;
+                avgrates(1,j) = sum(sum(actrate{j}))/(ge.Dv^2);                
             
                 % update 
                 if ~multistep
                     cholesky{j} = cholesky{j} + actrate{j} .* triu(grad{j});
                 end
             end    
+            avgrate = avgrate + sum(avgrates,2)/ge.k;    
+            
+            % multistep update
+            chol_cand = cholesky;
+            cdll = gestaltCompleteDataLogLikelihood(ge,samples(n,:,:),cholesky);
             if multistep
                 increase = true;
                 while increase
@@ -159,11 +163,7 @@ function [diff,longdiff] = gestaltIEM(ge,X,nSamples,maxStep,randseed,varargin)
                         chol_cand{j} = chol_cand{j} + actrate{j} .* triu(grad{j});
                     end
                     cdll_cand = gestaltCompleteDataLogLikelihood(ge,samples(n,:,:),chol_cand);
-%                     fprintf(' %f %f\n',cdll,cdll_cand);
-%                     pause;
-%                     for b=1:3+2*7
-%                         fprintf('\b');
-%                     end
+
                     if cdll_cand > cdll
                         cholesky = chol_cand;
                         cdll = cdll_cand;
@@ -172,75 +172,65 @@ function [diff,longdiff] = gestaltIEM(ge,X,nSamples,maxStep,randseed,varargin)
                     end
                 end
             end
+            
+            % update component matrices
             for j=1:ge.k
                 cc_next{j} = cholesky{j}' * cholesky{j};                                             
             end
+            ge = replaceComponents(ge,cc_next,precision);                                                        
             
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
-            % ACTUAL IEM PART ENDS HERE            
+            % PLOT, PRINT AND SAVE DATA            
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                        
+            
+            cdll_array(1,lidx) = cdll;
             
             if plot>1
                 [nopause,plot] = plotIEMStep(ge,cholesky,i,n,samples,grad,cc_next,cc_old,avgrates,nopause);
             end
             
             if nargout > 1
-                lidx = 1+(i-1)*ge.N+n;
-                [longdiff(1,lidx),minperm] = covcompRootMeanSquare(cc_next,ge.cc,minperm);
-            end
-            
-            % update parameters
-            if ~precision
-                ge.cc = cc_next;
-            else
-                ge.pc = cc_next;
-            end
+                [longdiff(1,lidx),minperm] = covcompRootMeanSquare(cc_next,cc_old,[]);
+            end                        
             
             if verb>1
-                for b=1:9+2*(floor(log10(nSamples))+1)
-                    fprintf('\b');
-                end
+                delPrint(nSamples);
             end
         end %for n=1:ge.N
+        avgrate = avgrate / ge.N;
         
         reldiff = covcompRootMeanSquare(cc_next,cc_prev,1:ge.k);
-        [diff(1,i+1),minperm] = covcompRootMeanSquare(cc_next,cc_old,minperm);        
+        [diff(1,i+1),minperm] = covcompRootMeanSquare(cc_next,cc_old,[]);        
         
-        if ~precision
-            pCC{i+1} = ge.cc;
-        else
-            pCC{i+1} = ge.pc;
-        end
+        pCC{i+1} = extractComponents(ge,precision);
         
         S{i} = samples;
-        save('iter.mat','pCC','S','mean_gradient');
+        save('iter.mat','pCC','S','mean_gradient','diff','longdiff','cdll_array');
         if verb>1
-            fprintf(' avglr %.2e diff %.2e skipped %d\n',sum(avgrates,2)/(ge.N*ge.k),reldiff,skipped);
+            fprintf(' avglr %.2e diff %.2e skipped %d\n',avgrate,reldiff,skipped);
         end
         
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+        % TEST FOR CONVERGENCE   
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        reldiff = covcompRootMeanSquare(cc_next,cc_prev,1:ge.k);
         if reldiff < 1e-3
             if verb>1
                 fprintf('Convergence achieved in %d steps.\n',i);
             end
             break;
         end
-    end
-        
-    ge.X = X_old;
-    dnum = ge.N;
-    ge.N = N_old;
-    
-    if ~precision
-        ge.pCC = ge.cc;        
-        ge.cc = cc_old;
-    else
-        ge.pPC = ge.pc;        
-        ge.pc = cc_old;
-    end
+    end        
+
     if plot>0
-        plotCovariances(ge,dnum,precision,[]);
+        plotCovariances(ge,ge.N,precision,[]);
     end
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+% FUNCTIONS FOR PLOTTING AND DATA ACCESS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
 function [nopause,plot] = plotIEMStep(ge,cholesky,i,n,samples,grad,cc_next,cc_old,avgrates,nopause)
     hor = 6;
@@ -287,5 +277,36 @@ function [nopause,plot] = plotIEMStep(ge,cholesky,i,n,samples,grad,cc_next,cc_ol
         elseif strcmp('r',ch)
             plot = 1;
         end
+    end
+end
+
+function ge = replaceComponents(ge,comps,precision)
+    if ~precision       
+        ge.cc = comps;
+    else      
+        ge.pc = comps;
+    end
+end
+
+function comps = extractComponents(ge,precision)
+    if ~precision
+        comps = ge.cc;        
+    else
+        comps = ge.pc;        
+    end
+end
+
+function [grand_avg,comp_avgs] = componentAbsMeans(cc)
+    k = size(cc,2);
+    comp_avgs = zeros(1,k);
+    for j=1:k
+        comp_avgs(1,j) = comp_avgs(1,j) + mean(mean(abs(cc{j}),2),1);
+    end
+    grand_avg = mean(comp_avgs,2);  
+end
+
+function delPrint(num)
+    for b=1:9+2*(floor(log10(num))+1)
+        fprintf('\b');
     end
 end
