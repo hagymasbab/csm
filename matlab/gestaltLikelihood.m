e7e0f6949ec791911410f4a092cd49162ee17f7f
Mihály Bányai <hagymasbab@gmail.com>
2014-05-08 15:32:59 +0200

    likelihood computation with separate exponents

--- matlab/gestaltLikelihood.m
+++ matlab/gestaltLikelihood.m
@@ -1,25 +1,50 @@
-function likelihood = gestaltLikelihood(ge,L)
+function [coefficient, exponent] = gestaltLikelihood(ge,L)
     % approximated, up to a constant
     % get L samples from a k-dimensional symmetric dirichelet prior for g
-    G = symmetricDirichlet(ge.sparsity,ge.k,L);
-    likelihood = 0;
-    coefficients = zeros(1,ge.N*ge.B);
-    exponents = zeros(1,ge.N*ge.B);
+    G = symmetricDirichlet(ge.sparsity,ge.k,ge.N*L);
+    coefficient = 0;
+    exponent = 0;
+    coeffs = zeros(L,ge.N*ge.B);
+    exps = zeros(L,ge.N*ge.B);
     for s=1:L        
-        Cv = componentSum(G(s,:)',ge.cc);
-        C = ge.obsVar * eye(ge.Dx) + ge.A * Cv * ge.A';
-        dataProb = 0;
         for n=1:ge.N
-            batchProb = 0;
+            g = G((s-1)*ge.N+n,:)';
+            Cv = componentSum(g,ge.cc);
+            C = ge.obsVar * eye(ge.Dx) + ge.A * Cv * ge.A';
             for b=1:ge.B                
                 x = squeeze(ge.X(n,b,:));                                
-                batchProb = batchProb + log(mvnpdf(x,zeros(size(x)),C));
-                %pause
+                p = mvnpdf(x,zeros(size(x)),C);
+                idx = (n-1) * ge.B + b;
+                [coeffs(s,idx),exps(s,idx)] = sciNot(p);                                
             end
-            batchProb = exp(batchProb);
-            dataProb = dataProb + batchProb;
-        end        
-        likelihood = likelihood + exp(dataProb);
+        end 
+        expsum = sum(exps(s,:),2);
+        coeffprod = prod(coeffs(s,:),2);
+        [co,ex] = sciNot(coeffprod);
+        expsum = expsum + ex;
+        if s==1
+            coefficient = co;
+            exponent = expsum;
+        else
+            [coefficient,exponent] = sumSciNot(coefficient,exponent,co,expsum);
+        end
     end
-    likelihood = likelihood / L;
+    % division by L could be done
+end
+
+function [coefficient,exponent] = sciNot(a)
+    [coefficient,exponent] = strread(strrep(sprintf('%E',a),'E','#'),'%f#%f');
+end
+
+function [coefficient,exponent] = sumSciNot(c1,e1,c2,e2)
+    exponent = max(e1,e2);
+    expdiff = abs(e1-e2);
+    if(e1 > e2)
+        coefficient = c1 + 10^(-expdiff) * c2;
+    else
+        coefficient = c2 + 10^(-expdiff) * c1;
+    end
+    [co,ex] = sciNot(coefficient);
+    coefficient = co;
+    exponent = exponent + ex;
 end
