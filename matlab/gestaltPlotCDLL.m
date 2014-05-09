7cb5e73bd546350543ef99483b76602bf1ba91e8
Mihály Bányai <hagymasbab@gmail.com>
2014-05-09 18:20:11 +0200

    likelihood corrected

--- matlab/gestaltPlotCDLL.m
+++ matlab/gestaltPlotCDLL.m
@@ -1,9 +1,18 @@
-function ll=gestaltPlotCDLL(ge,samples,gridnum,negative,ll)
+function [ll,samples] = gestaltPlotCDLL(ge,samples,gridnum,negative,ll)
     % only works for 2-dimensional, 1-component models
     x = linspace(-1*double(negative),1,gridnum);
     y = linspace(-1*double(negative),1,gridnum);
     z = linspace(-1*double(negative),1,gridnum);
     tics = linspace(-1*double(negative),1,4);
+    
+    if isscalar(samples)
+        nSamples = samples;
+        sdim = ge.k+(ge.Dv*ge.B);
+        samples = zeros(ge.N,nSamples,sdim);
+        for n=1:ge.N
+            [samples(n,:,:),~] = gestaltGibbs(ge,n,nSamples,'verbose',0);
+        end
+    end
     
     if ll==0
         ll = zeros(gridnum,gridnum,gridnum);
@@ -16,6 +25,7 @@ function ll=gestaltPlotCDLL(ge,samples,gridnum,negative,ll)
                     [~,err] = chol(ge.cc{1});
                     if err == 0 && rcond(ge.cc{1}) > 1e-16
                         ch = cellchol(ge.cc);
+                        %ll(i,j,k) = gestaltCompleteDataLogLikelihood(ge,samples,ch);
                         ll(i,j,k) = exp(gestaltCompleteDataLogLikelihood(ge,samples,ch));
                     else
                         ll(i,j,k) = 0;
@@ -26,6 +36,7 @@ function ll=gestaltPlotCDLL(ge,samples,gridnum,negative,ll)
         fprintf('\n');
     end        
     
+    clf;
     subplot(2,3,1);
     imagesc(sum(ll,3));
     xlabel('C(2,2)');
@@ -44,15 +55,19 @@ function ll=gestaltPlotCDLL(ge,samples,gridnum,negative,ll)
     plot(x,sum(sum(ll,3),2));
     xlabel('C(1,1)');
     ylim = get(gca,'YLim');
-    line([ge.cc{1}(1,1) ge.cc{1}(1,1)], [0 ylim(2)])
+    hold on
+    plot([ge.cc{1}(1,1) ge.cc{1}(1,1)], [0 ylim(2)],'r')
     subplot(2,3,5);
     plot(y,sum(sum(ll,3),1));
     xlabel('C(2,2)');
     ylim = get(gca,'YLim');
-    line([ge.cc{1}(2,2) ge.cc{1}(2,2)], [0 ylim(2)])
+    hold on
+    plot([ge.cc{1}(2,2) ge.cc{1}(2,2)], [0 ylim(2)],'r')
     subplot(2,3,6);
     plot(z,reshape(sum(sum(ll,1),2),1,gridnum));
     xlabel('C(1,2)');
     ylim = get(gca,'YLim');
-    line([ge.cc{1}(1,2) ge.cc{1}(1,2)], [0 ylim(2)])
+    hold on
+    plot([ge.cc{1}(1,2) ge.cc{1}(1,2)], [0 ylim(2)],'r')
+
     
