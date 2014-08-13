c6a5df88bce4406332f20e104e0f6f74d06362fd
Mihály Bányai <hagymasbab@gmail.com>
2014-08-13 18:03:13 +0200

    mean and variance change plotting

--- matlab/gestaltMeanVariance.m
+++ matlab/gestaltMeanVariance.m
@@ -9,9 +9,13 @@ function gestaltMeanVariance()
     x = ps(:);
     g1 = 0:0.1:1;
     pixel_index = 2 * sqrt(Dx) + 6;
+    other_index = 5 * sqrt(Dx) + 6; % this is inside another gestalt
     
     means = zeros(size(g1));
     variances = zeros(size(g1));
+    
+    other_means = zeros(size(g1));
+    other_variances = zeros(size(g1));
     
     for i=1:size(g1,2)
         g = [g1(i);1-g1(i)];
@@ -20,13 +24,24 @@ function gestaltMeanVariance()
         pv_mean = (1/delta_x) * pv_cov * A' * x;
         means(1,i) = pv_mean(pixel_index);
         variances(1,i) = pv_cov(pixel_index,pixel_index);
+        other_means(1,i) = pv_mean(other_index);
+        other_variances(1,i) = pv_cov(other_index,other_index);
     end
     
+    subplot(1,2,1);
     plot(g1,means);
     hold on;
-    plot(g1,variances,'r');
-    legend({'mean','variance'});
-    xlabel('Coefficient of relevant covariance component');
-    title('Conditional density parameters of a gestalt-activated V1 variable');
+    plot(g1,other_means,'r');
+    legend({'gestalt','other'});
+    xlabel('Coefficient of gestalt covariance component');
+    title('Means of V1 variables');
+    
+    subplot(1,2,2);
+    plot(g1,variances);
+    hold on;
+    plot(g1,other_variances,'r');
+    legend({'gestalt','other'});
+    xlabel('Coefficient of gestalt covariance component');
+    title('Variances of V1 variables');
 end
     
