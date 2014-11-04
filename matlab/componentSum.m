05f63481dcb3bc0b8d5c306e7560f008ff8166a0
Mihály Bányai <hagymasbab@gmail.com>
2014-11-04 18:55:54 +0100

    plotting learned parameters

--- matlab/componentSum.m
+++ matlab/componentSum.m
@@ -1,5 +1,7 @@
 function C = componentSum(coeffs,comps)
-    if size(coeffs,1) ~= size(comps,2)
+    if coeffs == 1
+        coeffs = ones(size(comps,2));
+    elseif size(coeffs,1) ~= size(comps,2)
         error('Number of coefficients (%d) is not equal to the numer of components (%d).\n',size(coeffs,1),size(comps,2));
     end
     C = zeros(size(comps{1}));
--- matlab/plotLearning.m
+++ matlab/plotLearning.m
@@ -1,101 +1,130 @@
-function plotLearning(ge1,ge2)
-    %pret: 1 2 - ford
-    %test: 1 3 4 - ford        
+function plotLearning(benchmarkName,goal,matchComponents)
+          
+    summedGoal = componentSum(1,goal);
+    Dv = size(goal{1},1);
+    k = size(goal,2);
     
-    cv1 = componentSum(ones(ge1.k,1),ge1.cc);
-    cv2 = componentSum(ones(ge2.k,1),ge2.cc);
+    if matchComponents
+        % TODO
+    end    
     
-    goals_all1 = repmat([ge1.cc{1}(:)' ge1.cc{1}(:)'],1,5);
-    goals_all2 = repmat([ge2.cc{1}(:)' ge2.cc{1}(:)'],1,5);
-    goals_sum1 = repmat(cv1(:)',1,5);
-    goals_sum2 = repmat(cv2(:)',1,5);
+    firstruns = dir(sprintf('%s_iter_param*_run1.mat',benchmarkName));
+    parNum = size(firstruns,1);
+    runfiles = dir(sprintf('%s_iter_param1_run*.mat',benchmarkName));  
+    runNum = size(runfiles,1);
     
-    starts_all1 = [];
-    starts_all2 = [];
-    starts_sum1 = [];
-    starts_sum2 = [];
+    summedGoalParams = repmat(summedGoal(:),runNum,1);
     
-    learned_all1 = [];
-    learned_all2 = [];
-    learned_sum1 = [];
-    learned_sum2 = [];
-    
-    for i=1:5
-        filename = sprintf('test_iter_param1_run%d.mat',i);
-        load(filename);
-        cc1 = state_sequence{1}.estimated_components{1};
-        cc2 = state_sequence{1}.estimated_components{2};
-        if i==1 || i==3 || i==4
-            temp = cc1;
-            cc1 = cc2;
-            cc2 = temp;
+    colNum = 1; % how many things will we plot for one parametrisation
+            
+    for act_param = 1:parNum
+        sumInitParams = zeros(Dv*Dv*runNum,1);
+        sumLearnedParams = zeros(Dv*Dv*runNum,1);
+        for f = 1:runNum
+            filename = sprintf('%s_iter_param%d_run%d.mat',benchmarkName,act_param,f);
+            load(filename);
+            initialComps = state_sequence{1}.estimated_components;
+            learnedComps = state_sequence{end}.estimated_components;
+            summedInitial = componentSum(1,initialComps);
+            summedLearned = componentSum(1,learnedComps);
+            sumInitParams((f-1)*Dv^2+1:f*Dv^2) = summedInitial(:);
+            sumLearnedParams((f-1)*Dv^2+1:f*Dv^2) = summedLearned(:);
+            if matchComponents
+                % TODO
+            end                        
         end
-        sumc = componentSum(ones(ge1.k,1),{cc1,cc2});
-        starts_all1 = [starts_all1 cc1(:)' cc2(:)'];
-        starts_sum1 = [starts_sum1 sumc(:)'];
         
-        cc1 = state_sequence{8}.estimated_components{1};
-        cc2 = state_sequence{8}.estimated_components{2};
-        if i==1 || i==3 || i==4
-            temp = cc1;
-            cc1 = cc2;
-            cc2 = temp;
+        subplot(parNum,colNum,(act_param-1)*colNum+1);
+        scatter(summedGoalParams,sumLearnedParams,'bo');
+        hold on;
+        scatter(summedGoalParams,sumInitParams,'gx');        
+        plot(xlim,xlim,'r-');
+        if act_param == 1
+            title('Summed parameters','FontSize',20);
+            legend({'learned','initial'},'FontSize',16);
         end
-        sumc = componentSum(ones(ge1.k,1),{cc1,cc2});
-        learned_all1 = [learned_all1 cc1(:)' cc2(:)'];
-        learned_sum1 = [learned_sum1 sumc(:)'];
-                
-        % -----------------------------------------------------------
+        xlabel('True value','FontSize',16);
+        ylabel('Estimated value','FontSize',16);
         
-        filename = sprintf('pret_iter_param1_run%d.mat',i);
-        load(filename);
-        cc1 = state_sequence{1}.estimated_components{1};
-        cc2 = state_sequence{1}.estimated_components{2};
-        if i==1 || i==3 || i==4
-            temp = cc1;
-            cc1 = cc2;
-            cc2 = temp;
-        end
-        sumc = componentSum(ones(ge2.k,1),{cc1,cc2});
-        starts_all2 = [starts_all2 cc1(:)' cc2(:)'];
-        starts_sum2 = [starts_sum2 sumc(:)'];
-        
-        cc1 = state_sequence{6}.estimated_components{1};
-        cc2 = state_sequence{6}.estimated_components{2};
-        if i==1 || i==2
-            temp = cc1;
-            cc1 = cc2;
-            cc2 = temp;
-        end
-        sumc = componentSum(ones(ge2.k,1),{cc1,cc2});
-        learned_all2 = [learned_all2 cc1(:)' cc2(:)'];
-        learned_sum2 = [learned_sum2 sumc(:)'];
     end
-       
-    subplot(2,3,1);
-    scatter(goals_all1,learned_all1);    
-    hold on;
-    scatter(goals_all1,starts_all1,'x');    
-    
-    subplot(2,3,2);
-    scatter(goals_sum1,learned_sum1);
-    hold on;
-    scatter(goals_sum1,starts_sum1,'x');
-    
-    subplot(2,3,4);
-    scatter(goals_all2,learned_all2);    
-    hold on;
-    scatter(goals_all2,starts_all2,'x');    
-    
-    subplot(2,3,5);
-    scatter(goals_sum2,learned_sum2);
-    hold on;
-    scatter(goals_sum2,starts_sum2,'x');
-    
-    subplot(2,3,3);
-    plotDifferences('test',5,1);
-    yl = ylim;
-    subplot(2,3,6);
-    plotDifferences('pret',5,1);
-    ylim(yl);
 end
+%     
+%     for i=1:5
+%         filename = sprintf('test_iter_param1_run%d.mat',i);
+%         load(filename);
+%         cc1 = state_sequence{1}.estimated_components{1};
+%         cc2 = state_sequence{1}.estimated_components{2};
+%         if i==1 || i==3 || i==4
+%             temp = cc1;
+%             cc1 = cc2;
+%             cc2 = temp;
+%         end
+%         sumc = componentSum(ones(ge1.k,1),{cc1,cc2});
+%         starts_all1 = [starts_all1 cc1(:)' cc2(:)'];
+%         starts_sum1 = [starts_sum1 sumc(:)'];
+%         
+%         cc1 = state_sequence{8}.estimated_components{1};
+%         cc2 = state_sequence{8}.estimated_components{2};
+%         if i==1 || i==3 || i==4
+%             temp = cc1;
+%             cc1 = cc2;
+%             cc2 = temp;
+%         end
+%         sumc = componentSum(ones(ge1.k,1),{cc1,cc2});
+%         learned_all1 = [learned_all1 cc1(:)' cc2(:)'];
+%         learned_sum1 = [learned_sum1 sumc(:)'];
+%                 
+%         % -----------------------------------------------------------
+%         
+%         filename = sprintf('pret_iter_param1_run%d.mat',i);
+%         load(filename);
+%         cc1 = state_sequence{1}.estimated_components{1};
+%         cc2 = state_sequence{1}.estimated_components{2};
+%         if i==1 || i==3 || i==4
+%             temp = cc1;
+%             cc1 = cc2;
+%             cc2 = temp;
+%         end
+%         sumc = componentSum(ones(ge2.k,1),{cc1,cc2});
+%         starts_all2 = [starts_all2 cc1(:)' cc2(:)'];
+%         starts_sum2 = [starts_sum2 sumc(:)'];
+%         
+%         cc1 = state_sequence{6}.estimated_components{1};
+%         cc2 = state_sequence{6}.estimated_components{2};
+%         if i==1 || i==2
+%             temp = cc1;
+%             cc1 = cc2;
+%             cc2 = temp;
+%         end
+%         sumc = componentSum(ones(ge2.k,1),{cc1,cc2});
+%         learned_all2 = [learned_all2 cc1(:)' cc2(:)'];
+%         learned_sum2 = [learned_sum2 sumc(:)'];
+%     end
+%        
+%     subplot(2,3,1);
+%     scatter(goals_all1,learned_all1);    
+%     hold on;
+%     scatter(goals_all1,starts_all1,'x');    
+%     
+%     subplot(2,3,2);
+%     scatter(goals_sum1,learned_sum1);
+%     hold on;
+%     scatter(goals_sum1,starts_sum1,'x');
+%     
+%     subplot(2,3,4);
+%     scatter(goals_all2,learned_all2);    
+%     hold on;
+%     scatter(goals_all2,starts_all2,'x');    
+%     
+%     subplot(2,3,5);
+%     scatter(goals_sum2,learned_sum2);
+%     hold on;
+%     scatter(goals_sum2,starts_sum2,'x');
+%     
+%     subplot(2,3,3);
+%     plotDifferences('test',5,1);
+%     yl = ylim;
+%     subplot(2,3,6);
+%     plotDifferences('pret',5,1);
+%     ylim(yl);
+% end
