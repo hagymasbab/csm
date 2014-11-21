0781e08a6e2ce8d90f73ba8fac5d6528c086f689
Mihály Bányai <hagymasbab@gmail.com>
2014-11-21 18:44:15 +0100

    cosyne abstract

--- matlab/tools/plotPair.m
+++ matlab/tools/plotPair.m
@@ -26,14 +26,14 @@ function plotPair(leftData,rightData,plotRows,plotColumns,leftPlotIndex,plotMean
     ylim([min(yl1(1),yl2(1)) max(yl1(2),yl2(2))]);
     plot([redLines{1};redLines{1}],ylim(),'r-');
     plot([redLines{1}+1;redLines{1}+1],ylim(),'k--');
-    plot([size(rightData,2)-redLines{2};size(rightData,2)-redLines{2}],ylim(),'r-');
-    plot([size(rightData,2)-redLines{2}-1;size(rightData,2)-redLines{2}-1],ylim(),'k--');
+    plot([size(rightData,2)-redLines{2}+1;size(rightData,2)-redLines{2}+1],ylim(),'r-');
+    plot([size(rightData,2)-redLines{2};size(rightData,2)-redLines{2}],ylim(),'k--');
     plot([1; size(rightData,2)],[0;0],'k--');
     subplot(plotRows,plotColumns,leftPlotIndex+1);
     ylim([min(yl1(1),yl2(1)) max(yl1(2),yl2(2))]);
     plot([redLines{1};redLines{1}],ylim(),'r-');
     plot([redLines{1}+1;redLines{1}+1],ylim(),'k--');
-    plot([size(rightData,2)-redLines{2};size(rightData,2)-redLines{2}],ylim(),'r-');
-    plot([size(rightData,2)-redLines{2}-1;size(rightData,2)-redLines{2}-1],ylim(),'k--');
+    plot([size(rightData,2)-redLines{2}+1;size(rightData,2)-redLines{2}+1],ylim(),'r-');
+    plot([size(rightData,2)-redLines{2};size(rightData,2)-redLines{2}],ylim(),'k--');
     plot([1; size(rightData,2)],[0;0],'k--');
 end
