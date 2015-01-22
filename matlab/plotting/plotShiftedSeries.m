49daa88af6c342eddd5bba8baf7ed8840f4a3ea1
Mihály Bányai <hagymasbab@gmail.com>
2015-01-22 12:24:54 +0100

    lots of plots

--- matlab/plotting/plotShiftedSeries.m
+++ matlab/plotting/plotShiftedSeries.m
@@ -5,11 +5,14 @@ function plotShiftedSeries(v)
             error('more than 2 dims in data');
         end
     end
+    
+    close all;
     nSeries = size(v,1);
     % step should be related to variance of the series
     step = max(abs(v(:)));
     %step = 1;
     shiftmat = repmat((0:step:(nSeries-1)*step)',1,size(v,2));
+    
     h = plot((v+shiftmat)');
     c = get(h,'Color');
     axis off;
@@ -29,4 +32,32 @@ function plotShiftedSeries(v)
         set(dh1,'Clipping','off');
         set(dh2,'Clipping','off');
     end
+    
+    sm_lvl = 15;
+    sv1 = smooth(v(1,:),sm_lvl,'loess');    
+    sv2 = smooth(v(2,:),sm_lvl,'loess');
+    sv3 = smooth(v(3,:),sm_lvl,'loess');
+    
+    figure;    
+    h2 = cline(sv1,sv2);
+    set(h2, 'LineWidth', 2);
+    colormap(copper);
+    grid on;
+    set(gca, 'XTickLabel', '');
+    set(gca, 'YTickLabel', '');
+    xlabel('V_m cell #1','FontSize',24);
+    ylabel('V_m cell #2','FontSize',24);
+    
+    figure;    
+    h3 = cline(sv1,sv2,sv3);
+    set(h3, 'LineWidth', 2);
+    colormap(copper);
+    grid on;
+    set(gca, 'XTickLabel', '');
+    set(gca, 'YTickLabel', '');
+    set(gca, 'ZTickLabel', '');
+    xlabel('V_m cell #1','FontSize',24);
+    ylabel('V_m cell #2','FontSize',24);
+    zlabel('V_m cell #3','FontSize',24);
+    view(3);
 end
