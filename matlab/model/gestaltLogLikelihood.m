5b6e6a1980db2b7059ae079b7289d1e18710c630
Mihály Bányai <hagymasbab@gmail.com>
2015-01-29 15:07:13 +0100

    likelihood computation

--- matlab/model/gestaltLogLikelihood.m
+++ matlab/model/gestaltLogLikelihood.m
@@ -1,6 +1,6 @@
 function ll = gestaltLogLikelihood(ge,L,data,cholesky)
     % approximated, up to a constant
-    % get L samples from a k-dimensional symmetric dirichelet prior for g
+    % get L samples from the priors of g and z
     
     if ~isempty(cholesky)
         for j=1:ge.k
@@ -9,88 +9,47 @@ function ll = gestaltLogLikelihood(ge,L,data,cholesky)
     end
     
     pA = pinv(ge.A);
-    iAA = inv(ge.A*ge.A');
+    siAA = ge.obsVar * inv(ge.A*ge.A');
     ll = 0;
-    if data == 0
-        nseq = 1:ge.N;
-        N = ge.N;
-    else
-        nseq = 1:data;
-        N = data;
-    end
-    G = symmetricDirichlet(ge.sparsity,ge.k,N*L);
+
+    G = gestaltSamplePriorG(ge,L);
+    Z = gamrnd(ge.z_shape,ge.z_scale,[L 1]);
     for i=1:N
-        n = nseq(i);
-        samp_coeffs = zeros(1,L);
-        samp_exps = zeros(1,L);
-        for s=1:L
-            batch_coeffs = zeros(1,ge.B);
-            batch_exps = zeros(1,ge.B);
-            g = G((i-1)*L+s,:)';
-            Cv = componentSum(g,ge.cc);
-            %C = ge.obsVar * eye(ge.Dx) + ge.A * Cv * ge.A';
-            C = ge.obsVar * iAA + Cv;
-            [~,err] = chol(C);
-            if err == 0 && isequal(C,C')                                          
-                for b=1:ge.B                
-                    x = squeeze(ge.X(n,b,:)); 
-                    x = pA * x;
-                    p = mvnpdf(x,zeros(size(x)),C);                               
-                    [batch_coeffs(1,b),batch_exps(1,b)] = sciNot(p);                                
-                end            
+        % TODO B > 1
+        x = data(i,:)';
+        Ax = pA * x;
+        act_L = 0;
+        for sz=1:L
+            cov_left = siAA / Z(sz,1)^2;
+            eval_site = Ax / Z(sz,1);
+            act_LG = 0;
+            for sg=1:L
+                g = G(sg,:)';
+                cov_full = cov_left + componentSum(g,ge.cc);
+                act_LG = act_LG + normpdf(eval_site,0,cov_full);
             end
-            [samp_coeffs(1,s),samp_exps(1,s)] = sciProd(batch_coeffs,batch_exps);
-        end 
-        [datum_coeff,datum_exp] = sciSum(samp_coeffs,samp_exps);
-        ll = ll + log10(datum_coeff) + datum_exp;       
-    end
-end
-
-function [coefficient,exponent] = sciNot(a)
-    [coefficient,exponent] = strread(strrep(sprintf('%E',a),'E','#'),'%f#%f');
-end
-
-function [coefficient,exponent] = sciSum(coeffs,exps)
-    exponent = 0;
-    coefficient = 0;
-    for i=1:size(coeffs,2)
-        [coefficient,exponent] = sumSciNot(coefficient,exponent,coeffs(1,i),exps(1,i));
-    end
-end
-
-function [coefficient,exponent] = sumSciNot(c1,e1,c2,e2)    
-    if c1 == 0 && e1 == 0
-        coefficient = c2;
-        exponent = e2;
-        return;
-    elseif c2 == 0 && e2 == 0
-        coefficient = c1;
-        exponent = e1;
-        return;
-    end
-    exponent = max(e1,e2);
-    expdiff = abs(e1-e2);
-    if(e1 > e2)
-        coefficient = c1 + 10^(-expdiff) * c2;
-    else
-        coefficient = c2 + 10^(-expdiff) * c1;
-    end
-    [coefficient,ex] = sciNot(coefficient);    
-    exponent = exponent + ex;
-end
-
-function [coefficient,exponent] = sciProd(a,e)
-    exponent = 0;
-    coefficient = 1;
-    for i=1:size(a,2)        
-        coefficient = coefficient * a(i);
-        if coefficient >= 10
-            exponent = exponent + 1;
-            coefficient = coefficient / 10;
-        elseif coefficient <= -10
-            exponent = exponent - 1;
-            coefficient = coefficient / 10;
+            act_L = act_L + act_LG / Z(sz,1)^(ge.Dv);
         end
-    end    
-    exponent = exponent + sum(e,2);
+        ll = ll + log(act_L);
+    end
+%             batch_coeffs = zeros(1,ge.B);
+%             batch_exps = zeros(1,ge.B);
+%             g = G((i-1)*L+s,:)';
+%             Cv = componentSum(g,ge.cc);
+%             %C = ge.obsVar * eye(ge.Dx) + ge.A * Cv * ge.A';
+%             C = ge.obsVar * iAA + Cv;
+%             [~,err] = chol(C);
+%             if err == 0 && isequal(C,C')                                          
+%                 for b=1:ge.B                
+%                     x = squeeze(ge.X(n,b,:)); 
+%                     x = pA * x;
+%                     p = mvnpdf(x,zeros(size(x)),C);                               
+%                     [batch_coeffs(1,b),batch_exps(1,b)] = sciNot(p);                                
+%                 end            
+%             end
+%             [samp_coeffs(1,s),samp_exps(1,s)] = sciProd(batch_coeffs,batch_exps);
+%         end 
+%         [datum_coeff,datum_exp] = sciSum(samp_coeffs,samp_exps);
+%         ll = ll + log10(datum_coeff) + datum_exp;       
+%     end
 end
--- matlab/model/gestaltSamplePriorG.m
+++ matlab/model/gestaltSamplePriorG.m
@@ -1,33 +1,37 @@
-function g_act = gestaltSamplePriorG(ge,distribution,varargin)
+function G = gestaltSamplePriorG(ge,L,varargin)
     parser = inputParser;
     addParamValue(parser,'sampleRetry',10,@isnumeric);
     addParamValue(parser,'precision',false,@islogical);
     parse(parser,varargin{:});
     params = parser.Results;
     
-    valid = false;
-    tries = 0;
-    while ~valid
-        if strcmp(distribution,'gamma')
-            g_act = zeros(ge.k,1);
-            for d = 1:ge.k            
-                if ge.nullComponent && d == ge.k
-                    g_act(d,1) = gamrnd(ge.null_shape,ge.null_scale);
-                else
-                    g_act(d,1) = gamrnd(ge.g_shape,ge.g_scale);
+    G = zeros(L,ge.k);
+    for i=1:L
+        valid = false;
+        tries = 0;
+        while ~valid
+            if strcmp(ge.prior,'gamma')
+                g_act = zeros(ge.k,1);
+                for d = 1:ge.k            
+                    if ge.nullComponent && d == ge.k
+                        g_act(d,1) = gamrnd(ge.null_shape,ge.null_scale);
+                    else
+                        g_act(d,1) = gamrnd(ge.g_shape,ge.g_scale);
+                    end
                 end
+            elseif strcmp(ge.prior,'dirichlet')
+                 g_act = symmetricDirichlet(ge.sparsity,ge.k,1)';
+            else
+                error('Prior distribution %s not implemented',ge.prior);
             end
-        elseif strcmp(distribution,'dirichlet')
-             g_act = symmetricDirichlet(ge.sparsity,ge.k,1)';
-        else
-            error('Prior distribution %s not implemented',distribution);
-        end
 
-        valid = gestaltCheckG(g_act,ge,params.precision);
-        tries = tries + 1;
-        % if we cannot find a valid g, return an error code
-        if tries > params.sampleRetry
-            throw(MException('Gestalt:Prior:TooManyTries',sprintf('Number of tries to draw a valid g vector from the prior exceeded %d',params.sampleRetry)));            
+            valid = gestaltCheckG(g_act,ge,params.precision);
+            tries = tries + 1;
+            % if we cannot find a valid g, return an error code
+            if tries > params.sampleRetry
+                throw(MException('Gestalt:Prior:TooManyTries',sprintf('Number of tries to draw a valid g vector from the prior exceeded %d',params.sampleRetry)));            
+            end
         end
+        G(i,:) = g_act';
     end
 end
