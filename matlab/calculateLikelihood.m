01a8ad3670ded57536e10755b75fb68c00f4dad2
Mihály Bányai <hagymasbab@gmail.com>
2015-02-04 16:48:59 +0100

    matrix norms

--- matlab/calculateLikelihood.m
+++ matlab/calculateLikelihood.m
@@ -1,4 +1,4 @@
-function calculateLikelihood(iterfile,gefile,dataset,N_test,samples,randseed)
+function calculateLikelihood(iterfile,gefile,dataset,N_test,samples,randseed,calculate)
 
     setrandseed(randseed);
 
@@ -23,14 +23,30 @@ function calculateLikelihood(iterfile,gefile,dataset,N_test,samples,randseed)
         end
     end
     
+    
     likelihoods = zeros(index,1);
+    norms = zeros(index,ge.k);
+    distances = zeros(index,ge.k*(ge.k-1)/2);
     
     for i=1:index
         printCounter(i,'stringVal','Iteration','maxVal',46);
         act_cc = state_sequence{i}.estimated_components;
-        ge.cc = act_cc;
-        ll = gestaltLogLikelihood(ge,samples,X_test,'scientific',true);        
-        likelihoods(i) = ll;
-        save('iter_likes.mat','likelihoods');
+        if strcmp(calculate,'likelihood')
+            ge.cc = act_cc;
+            ll = gestaltLogLikelihood(ge,samples,X_test,'scientific',true);        
+            likelihoods(i) = ll;
+            save('iter_likes.mat','likelihoods');
+        elseif strcmp(calculate,'metrics')
+            met_idx = 1;
+            for kk = 1:ge.k
+              norms(i,kk) = norm(act_cc{kk});
+              for other = kk+1:ge.k
+                  dist = covcompRootMeanSquare(act_cc(kk),act_cc(other),[1]);
+                  distances(i,met_idx) = dist;
+                  met_idx = met_idx+1;
+              end
+            end
+            save('iter_norms.mat','norms','distances');
+        end
     end
 end
