4551302dca7d789e5522afdffffb9e55b9ef4d6f
Mihály Bányai <hagymasbab@gmail.com>
2015-02-05 15:55:26 +0100

    s

--- matlab/tools/stableMvnpdf.m
+++ matlab/tools/stableMvnpdf.m
@@ -7,7 +7,7 @@ function [p,pexp] = stableMvnpdf(x,mu,C,scientific)
     ld = -ld/2;
     v = x-mu;
     rc = rcond(C);
-    if log10(rc) < -30
+    if log10(rc) < -15
         quad = v' * pinv(C) * v;
     else
         quad = v' * (C \ v);
