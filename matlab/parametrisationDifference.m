17b4c612a248621298b0b889ee76cb45b563463e
Mihály Bányai <banyai.mihaly@wigner.mta.hu>
2015-02-21 17:39:49 +0100

    KL parametrisation components

---
+++ matlab/parametrisationDifference.m
@@ -0,0 +1,53 @@
+function bcc = parametrisationDifference(ge,cc,loaddata)
+    if loaddata
+        load('paramdiff.mat');
+    else
+        ccm = cell(1,length(cc));
+        bcm = cell(1,length(cc));
+        bcc = cell(1,length(cc));
+        for i=1:length(cc)
+            b = sign(cc{i}(1,:))' .* sqrt(diag(cc{i}));
+            bcc{i} = b*b';
+            ccm{i} = corrcov(cc{i});
+            bcm{i} = corrcov(bcc{i});
+        end
+    %     viewImageSet(bcc)
+
+        [~,seeds,~,cst] = gestaltGReceptiveFields(ge,cc,10000,false);
+        [~,bseeds,~,bst] = gestaltGReceptiveFields(ge,bcc,10000,false);
+        save('paramdiff.mat','bcc','seeds','bseeds','cst','bst','cc','ccm','bcm');
+    end
+    hornum = 6;
+    subplot = @(m,n,p) subtightplot (m, n, p, [0.025 0.001], [0 0.025], [0 0.01]);
+    load cmp_diff
+    for i=1:length(cc)      
+        printCounter(i,'maxVal',length(cc),'stringVal','Component');
+        diffcm = ccm{i} - bcm{i};
+        diffst = cst{i} - bst{i};
+        
+        um = true;
+        subplot(length(cc),hornum,(i-1)*hornum + 1);
+        viewImage(ccm{i},'useMax',um);
+        freezeColors
+        subplot(length(cc),hornum,(i-1)*hornum + 2);
+        viewImage(bcm{i},'useMax',um);
+        freezeColors
+        subplot(length(cc),hornum,(i-1)*hornum + 3);
+        viewImage(diffcm,'useMax',um);
+        colormap(cmp_diff);
+        freezeColors
+        
+        subplot(length(cc),hornum,(i-1)*hornum + 4);
+        viewImage(cst{i},'useMax',um);
+        freezeColors
+        subplot(length(cc),hornum,(i-1)*hornum + 5);
+        viewImage(bst{i},'useMax',um);
+        freezeColors
+        subplot(length(cc),hornum,(i-1)*hornum + 6);
+        viewImage(diffst,'useMax',um);
+        colormap(cmp_diff);
+        freezeColors
+    end
+end
+        
+        
