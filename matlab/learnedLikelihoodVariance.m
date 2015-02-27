22ccfcf4ba9d6e5679ff555923cc13c7ab88903c
Mihály Bányai <banyai.mihaly@wigner.mta.hu>
2015-02-27 23:31:34 +0100

    likevar

---
+++ matlab/learnedLikelihoodVariance.m
@@ -0,0 +1,23 @@
+function learnedLikelihoodVariance(ge,cc,N_test,nTrial,samplenums,loadData,plotStuff)
+    load('patches_vanhateren_576.mat'); % TODO gerealise
+    N_all = size(patchDB,2);
+    X_test = patchDB(:,chooseKfromN(N_test,N_all))';
+    ge.cc = cc;
+    if loadData
+        load('bin/save_likevar.mat');
+    else
+        ll = zeros(nTrial,length(samplenums));
+        for s = 1:length(samplenums)
+            for t = 1:nTrial
+                ll(t,s) = gestaltLogLikelihood(ge,samplenums(s),X_test,'scientific',true); 
+            end
+        end
+        save('bin/save_likevar.mat','ll');
+    end
+    
+    if plotStuff
+        barwitherr(std(ll)',mean(ll)');
+        xlabels={};for i=1:length(samplenums);xlabels{end+1}=sprintf('%d',samplenums(i));end;
+        set(gca,'XTickLabel',xlabels)
+    end
+end
