8ca4f65b32bdb2fdb47c1dda8fca434fd66af721
Mihály Bányai <hagymasbab@gmail.com>
2015-04-29 18:11:50 +0200

    matrix form component summation

--- matlab/tools/componentSum.m
+++ matlab/tools/componentSum.m
@@ -4,8 +4,13 @@ function C = componentSum(coeffs,comps)
     elseif size(coeffs,1) ~= size(comps,2)
         error('Number of coefficients (%d) is not equal to the numer of components (%d).\n',size(coeffs,1),size(comps,2));
     end
-    C = zeros(size(comps{1}));
-    for i=1:size(coeffs,1)
-        C = C + coeffs(i,1) * comps{i};
+    if iscell(comps)
+        C = zeros(size(comps{1}));
+        for i=1:size(coeffs,1)
+            C = C + coeffs(i,1) * comps{i};
+        end
+    else
+        compdim = sqrt(size(comps,1));
+        C = reshape(comps * coeffs,compdim,compdim);
     end
 end
--- matlab/tools/test_timing.m
+++ matlab/tools/test_timing.m
@@ -1,27 +1,33 @@
 function test_timing()
 
     c = rand(576);
+    c = c*c';
     v = rand(576,1);    
 
     a = @() invert_multiply(c,v);
     b = @() invert_multiply2(c,v);
     %b = @() divide(c,vv);
     
+    r1 = invert_multiply(c,v);
+    r2 = invert_multiply2(c,v);
+    isequal(r1,r2)
+    viewImage(r1-r2,'useMax',false);
+    
     timeit(a)
     timeit(b)
     
 end
 
-function invert_multiply(c,v)
+function ret = invert_multiply(c,v)
     vv = v * v';
     ic = inv(c);
-    ic * vv * ic;
+    ret = ic * vv * ic;
 end
 
-function invert_multiply2(c,v)
+function ret = invert_multiply2(c,v)
     ic = inv(c);
     m1 = ic * v;
-    m1*m1';
+    ret = m1*m1';
 end
 
 function divide(c,vv)
