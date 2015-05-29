2c953bd23c98da168cd27e9a232c0983c9b12dd0
Mihály Bányai <hagymasbab@gmail.com>
2015-05-29 19:37:23 +0200

    gradient comp

--- matlab/model/gestaltLogLikelihoodGradient.m
+++ matlab/model/gestaltLogLikelihoodGradient.m
@@ -3,6 +3,7 @@ function grad = gestaltLogLikelihoodGradient(ge,L,data,cholesky,varargin)
     addParameter(parser,'verbose',0,@isnumeric);  
     addParameter(parser,'loadSamples',false,@islogical);
     addParameter(parser,'randseed','leave');      
+    addParameter(parser,'method','standard');      
     parse(parser,varargin{:});        
     params = parser.Results;  
     
@@ -33,7 +34,9 @@ function grad = gestaltLogLikelihoodGradient(ge,L,data,cholesky,varargin)
     
     covariances = zeros(L,ge.Dv,ge.Dv);
     inverse_covariances = zeros(L,ge.Dv,ge.Dv);
+    % TODO csak loghz kell
     hz = zeros(L,1);
+    loghz = zeros(L,1);
     parfor l=1:L
         g_l = G(l,:)';
         z_l = Z(l,1);
@@ -41,24 +44,25 @@ function grad = gestaltLogLikelihoodGradient(ge,L,data,cholesky,varargin)
         leftmat = siATA / Z2(l);
         nCl = leftmat + cv;                    
         Cl = nearestSPD(nCl);        
-        [~,e1] = cholcov(leftmat);
-        [~,e2] = cholcov(cv);
-        [~,e3] = cholcov(nCl);
-        [~,e4] = cholcov(Cl);
-        fprintf('Condition siATA/z %e Cv %e nCl %e Cl %e\n',rcond(leftmat),rcond(cv),rcond(nCl),rcond(Cl));
-        fprintf('Posdef siATA/z %d Cv %d nCl %d Cl %d\n',e1,e2,e3,e4);
+%         [~,e1] = cholcov(leftmat);
+%         [~,e2] = cholcov(cv);
+%         [~,e3] = cholcov(nCl);
+%         [~,e4] = cholcov(Cl);
+%         fprintf('Condition siATA/z %e Cv %e nCl %e Cl %e\n',rcond(leftmat),rcond(cv),rcond(nCl),rcond(Cl));
+%         fprintf('Posdef siATA/z %d Cv %d nCl %d Cl %d\n',e1,e2,e3,e4);
         covariances(l,:,:) = Cl;
         inverse_covariances(l,:,:) = stableInverse(reshape(covariances(l,:,:),ge.Dv,ge.Dv));
         %inverse_covariances(l,:,:) = inv(reshape(covariances(l,:,:),ge.Dv,ge.Dv));
         %hz(l) = idATA / (z_l^ge.Dv);
         hz(l) = 1 / (z_l^ge.Dv);
+        loghz(l) = -ge.Dv * log(z_l);
     end    
     if any(isnan(hz))
         error('NaN in hz');
     end
     %Z.^ge.Dv
     %Z
-    %hz
+    %loghz
         
     M = zeros(ge.k,ge.Dv,ge.Dv);
     parfor n = 1:N
@@ -67,55 +71,94 @@ function grad = gestaltLogLikelihoodGradient(ge,L,data,cholesky,varargin)
         end
         x_n = data(n,:)';
         pAx = pA * x_n;
-        Li_n = 0;
+        
+        if strcmp(params.method,'scinot')
+            % first calculate h_l * N_l for numerical stability
+            h_times_N_coeff = zeros(L,1);        
+            h_times_N_expo = zeros(L,1);        
+            Li_coeff = 0;
+            Li_expo = 0;
+            for l = 1:L
+                f_l = pAx / Z(l);
+                iC_l = reshape(inverse_covariances(l,:,:),ge.Dv,ge.Dv);
+                [coeff_n,expo_n] = stableMvnpdf(f_l,zeros(ge.Dv,1),iC_l,true,true);  
+                %expo_n
+                %[coeff_h,expo_h] = sciNot(hz(l),false);
+                [coeff_h,expo_h] = sciNot(loghz(l),true);   
+                %expo_h
+                [h_times_N_coeff(l),h_times_N_expo(l)] = prodSciNot([coeff_n coeff_h],[expo_n expo_h]);
+                [Li_coeff,Li_expo] = sumSciNot(Li_coeff,Li_expo,h_times_N_coeff(l),h_times_N_expo(l));
+            end        
+    %         h_times_N_coeff
+    %         h_times_N_expo
+    %         
+    %         Li_coeff
+        else
+            Li_n = 0;        
+        end
+        
         M_part = zeros(ge.k,ge.Dv,ge.Dv);
         zero_scalars = 0;
+        
         for l = 1:L      
-            %printCounter((n-1)*L+l,'maxVal',N*L,'stringVal','e')
-            f_l = pAx / Z(l);
-            C_l = reshape(covariances(l,:,:),ge.Dv,ge.Dv);
-            iC_l = reshape(inverse_covariances(l,:,:),ge.Dv,ge.Dv);
-            iCf = iC_l * f_l;
+            if strcmp(params.method,'scinot')
+                [hNperL_coeff,hNperL_expo] = prodSciNot([h_times_N_coeff(l) 1/Li_coeff],[h_times_N_expo(l) -Li_expo]);
+                hNperL = hNperL_expo
+%                 if l==6
+%                     h_times_N_coeff(l)
+%                     h_times_N_expo(l)
+%                     1/Li_coeff
+%                     -Li_expo
+%                     hNperL_coeff
+%                     hNperL_expo
+%                 end
+                % TODO scalar_term szamolasa a scinotos cuccal legyen
+            else
             
-%             size(C_l)
-%             issymmetric(C_l)
-%              [~,e] = cholcov(C_l);
-%              e
-%              [~,e] = cholcov(iC_l);
-%              e                                            
+                %printCounter((n-1)*L+l,'maxVal',N*L,'stringVal','e')
+                f_l = pAx / Z(l);
+                C_l = reshape(covariances(l,:,:),ge.Dv,ge.Dv);
+                iC_l = reshape(inverse_covariances(l,:,:),ge.Dv,ge.Dv);
+                iCf = iC_l * f_l;                                          
+
+                N_f = mvnpdf(f_l',zeros(1,ge.Dv),C_l);
+                [a,e] = stableMvnpdf(f_l,zeros(ge.Dv,1),iC_l,true,true);            
+                %e
+
+                if any(isnan(N_f))
+                    error('NaN in N_f,n %d %d',n,l);
+                end
+                %N_f = stableMvnpdf(f_l,zeros(ge.Dv,1),C_l,false,false)
+                
+                if N_f == 0                
+                    scalar_term = 0;
+                    zero_scalars = zero_scalars + 1;
+                else                
+                    scalar_term = hz(l) * N_f;   
+                end
+                if any(isnan(scalar_term))
+                    hz(l)
+                    N_f
+                    error('NaN in scalar,n %d %d',n,l);
+                end
+                
+                 Li_n = Li_n + scalar_term;  
+            end
             
-            N_f = mvnpdf(f_l',zeros(1,ge.Dv),C_l);
-            [a,e] = stableMvnpdf(f_l,zeros(ge.Dv,1),iC_l,true,true);            
-            %e
-            
-            if any(isnan(N_f))
-                error('NaN in N_f,n %d %d',n,l);
-            end
-            %N_f = stableMvnpdf(f_l,zeros(ge.Dv,1),C_l,false,false)
-            if N_f == 0                
-                scalar_term = 0;
-                zero_scalars = zero_scalars + 1;
-            else                
-                scalar_term = hz(l) * N_f;   
-            end
-            if any(isnan(scalar_term))
-                hz(l)
-                N_f
-                error('NaN in scalar,n %d %d',n,l);
-            end
             matrix_term = iC_l - iCf * iCf';
             
             for kk = 1:ge.k
                 M_part(kk,:,:) = M_part(kk,:,:) + reshape(G(l,kk) * scalar_term * matrix_term,1,ge.Dv,ge.Dv);
-            end
-            
-            Li_n = Li_n + scalar_term;            
+            end                                                                
         end
+        
         zero_scalars
         % this is as approximation
-        if Li_n ~= 0
-            M = M + M_part / Li_n;      
+        if ~strcmp(params.method,'scinot') && Li_n ~= 0
+            M_part = M_part / Li_n;      
         end
+        
+        M = M + M_part;
     end
     
     if any(isnan(M))
