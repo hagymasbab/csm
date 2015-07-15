ac0fc731b807d5ad44cbbeebb77b706c03e73cfa
Mihály Bányai <banyai.mihaly@wigner.mta.hu>
2015-07-15 19:13:37 +0200

    dfg3

--- matlab/results/contrastMeanCov.m
+++ matlab/results/contrastMeanCov.m
@@ -58,7 +58,7 @@ function contrastMeanCov(randseed,loadStuff,plotStuff)
     
     % create contrast-adjusted stimuli
     %contrasts = [0.01 0.1 0.5 1 2 5 10 20 100];
-    contrasts = [0.05 0.1 0.5 1 5 10];
+    contrasts = [0.1 0.5 1 5];
     
     x_rms = zeros(length(contrasts),1);
 %     corr_c = zeros(length(contrasts),1);
