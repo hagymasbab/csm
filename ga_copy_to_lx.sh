578bdafd895b585f3aa876c8668bd88cbe050b1a
Mihály Bányai <hagymasbab@gmail.com>
2015-08-23 10:37:26 +0200

    copying script for GA result files

---
+++ ga_copy_to_lx.sh
@@ -0,0 +1,3 @@
+scp /home/banmi/a1_local/csm/matlab/bin/gradasc_iter.mat banmi@lxserv.wigner.mta.hu:~/gradasc_a1.mat
+scp /home/umat/misi/f4_local/csm/matlab/bin/gradasc_iter.mat banmi@lxserv.wigner.mta.hu:~/gradasc_f4.mat
+scp /home/umat/misi/csm/matlab/bin/gradasc_iter.mat banmi@lxserv.wigner.mta.hu:~/gradasc_q10.mat
