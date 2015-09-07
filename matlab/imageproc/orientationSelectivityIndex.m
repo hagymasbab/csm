774ff9adbddebc0c5d0c2a749dae1fb5c84758f1
Mihály Bányai <hagymasbab@gmail.com>
2015-09-07 18:44:47 +0200

    firing rate nonlinearitiy in OSI computation

--- matlab/imageproc/orientationSelectivityIndex.m
+++ matlab/imageproc/orientationSelectivityIndex.m
@@ -1,4 +1,10 @@
-function OSI = orientationSelectivityIndex(tuningCurve)
+function OSI = orientationSelectivityIndex(tuningCurve,FRnonlin)
+
+    if FRnonlin
+        % using parameter values from Carandini, 2004, PLoS Biol
+        tuningCurve = 10 * max(tuningCurve - 1.9,0).^(1.1);
+    end
+        
     thetaVals = linspace(0,180,length(tuningCurve));
     [maxResp,maxIndex] = max(tuningCurve);
     prefOrient = tuningCurve(maxIndex);
