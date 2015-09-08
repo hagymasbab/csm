dd0ef68ecd3ef6c0466920d41c0572081eb7e214
Mihály Bányai <banyai.mihaly@wigner.mta.hu>
2015-09-08 11:30:06 +0200

    random testing

--- matlab/gsm/gsmOrientSelEM.m
+++ matlab/gsm/gsmOrientSelEM.m
@@ -9,22 +9,25 @@ function gsmOrientSelEM(iterfile,loadStuff,posterior,thetaRes,FRnonlin)
     else        
         tuning_curves = zeros(nStep,nFilt,thetaRes);
         respVar = zeros(nStep,nFilt);
+        prefThetas = zeros(nStep,nFilt);
         for i=1:nStep        
             [prefTheta,tuningCurves,prefPhase,prefLambda,prefVars] = gsmOrientationSelectivity(A_iter{i},C,sigma_iter(i),thetaRes,false,'grating',~posterior,1,false);
+            prefThetas(i,:) = prefTheta;
+            %pause
             tuning_curves(i,:,:) = tuningCurves;
             if posterior
                 respVar(i,:) = prefVars;
             end
             for j = 1:nFilt                                
                 if ~posterior
-                    printCounter(j,'StringVal','Preferred posterior','maxVal',nFilt);
-                    prefGrat = grating(prefLambda(j),prefTheta(j),prefPhase(j),imsize);
-                    [~,C_post,~,~] = gsmPosteriorV(prefGrat(:),A_iter{i},C,sigma_iter(i),2,2,10);
-                    respVar(i,j) = C_post(j,j);
+%                     printCounter(j,'StringVal','Preferred posterior','maxVal',nFilt);
+%                     prefGrat = grating(prefLambda(j),prefTheta(j),prefPhase(j),imsize);
+%                     [~,C_post,~,~] = gsmPosteriorV(prefGrat(:),A_iter{i},C,sigma_iter(i),2,2,10);
+%                     respVar(i,j) = C_post(j,j);
                 end
             end
         end
-        save('bin/save_emorient.mat','respVar','tuning_curves');
+        save('bin/save_emorient.mat','respVar','tuning_curves','prefThetas');
     end
     
     OSIs = zeros(nStep,nFilt);
@@ -32,6 +35,9 @@ function gsmOrientSelEM(iterfile,loadStuff,posterior,thetaRes,FRnonlin)
         for j = 1:nFilt                
             OSIs(i,j) = orientationSelectivityIndex(tuning_curves(i,j,:),FRnonlin);
         end
+        hist(prefThetas(i,:),linspace(0,180,100));
+        title(sprintf('EM step %d',em_steps(i)))
+        pause
     end
     
     close all;
@@ -39,10 +45,10 @@ function gsmOrientSelEM(iterfile,loadStuff,posterior,thetaRes,FRnonlin)
     xlim([0 em_steps(nStep)+20]);
     xlabel('EM step #','FontSize',16);
     ylabel('OSI mean and st.d.','FontSize',16);
-    figure;
-    errorbar(em_steps,mean(respVar,2),std(respVar,0,2),'LineWidth',2);
-    xlim([0 em_steps(nStep)+20]);
-    xlabel('EM step #','FontSize',16);
-    ylabel('Response variance to pref. stim., mean and st.d.','FontSize',16);
+%     figure;
+%     errorbar(em_steps,mean(respVar,2),std(respVar,0,2),'LineWidth',2);
+%     xlim([0 em_steps(nStep)+20]);
+%     xlabel('EM step #','FontSize',16);
+%     ylabel('Response variance to pref. stim., mean and st.d.','FontSize',16);
 end
         
