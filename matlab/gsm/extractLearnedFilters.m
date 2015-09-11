d523f7e6b9ae0000a15f7f59c41fcbf616669259
Mihály Bányai <hagymasbab@gmail.com>
2015-09-11 10:30:32 +0200

    extracting filters from the new run

--- matlab/gsm/extractLearnedFilters.m
+++ matlab/gsm/extractLearnedFilters.m
@@ -1,13 +1,20 @@
 %cd /home/umat/ogergo/a_learn_data
 cd /home/banmi/gsm/run2
 files = dir('*.mat');
+
+cycles_steps = [1 3 4 5 6; 10 8 20 25 30];
+
 A_iter = {};
 sigma_iter = [];
 em_steps = [];
-for i=1:length(files)
-    actFile = files(i);
-    load(actFile.name);
-    splitName = strsplit(actFile.name,'_');
+nFiles = size(cycles_steps,2);
+for i=1:nFiles
+    actname = sprintf('natim_learnA_2_dx256_du248_dz50_normem0_restart2k4_cycle%d_em%d.mat',cycles_steps(1,i),cycles_steps(2,i))
+    %actFile = files(i);
+    %load(actFile.name);
+    load(actname)
+    %splitName = strsplit(actFile.name,'_');
+    splitName = strsplit(actname,'_');
     stepNum = str2num(splitName{end}(3:end-4));
 %     if strcmp(splitName{end-2},'restart2k4')
 %         stepNum = stepNum + 2400;
@@ -54,4 +61,4 @@ sigma_iter = sigma_iter(sortIdx);
 A_iter = A_iter(sortIdx);
         
 cd /home/banmi/csm/matlab
-save('bin/A_gsm_iter3.mat','C','A_iter','sigma_iter','em_steps');
+save('bin/A_gsm_iter4.mat','C','A_iter','sigma_iter','em_steps');
