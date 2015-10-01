631596053f1de2745c1c72de3cf10344f0aabe79
Banyai Mihaly <banmi@m3.rmki.kfki.hu>
2015-10-01 16:29:26 +0200

    printCounter standardized

--- matlab/tools/printCounter.m
+++ matlab/tools/printCounter.m
@@ -1,8 +1,18 @@
 function printCounter(i,varargin)
+% printCounter - displays a simple counter within a for loop
+% usage: for i=1:imax; printCounter(i,'maxVal',imax,'stringVal','Iteration','newLine',true); end
+%
+% Arguments:
+%   i (integer) - the loop variable
+%   Optional:
+%     'maxVal' (integer) - see the coutner in a form maxVal/i, e.g. 100/23
+%     'stringVal' (string) - display a string in front of the counter, e.g. Iter 100/23
+%     'newLine' (logical) - whether to put the cursor in a new line at the end of the loop
+
     parser = inputParser;    
-    addParamValue(parser,'maxVal',0,@isnumeric);
-    addParamValue(parser,'stringVal','');
-    addParamValue(parser,'newLine',true,@islogical);
+    addParameter(parser,'maxVal',0,@isnumeric);
+    addParameter(parser,'stringVal','');
+    addParameter(parser,'newLine',true,@islogical);
     parse(parser,varargin{:});
     params = parser.Results;
     
