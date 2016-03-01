d2413a27a5b0ee96ee0bb1f013b1b7507028b919
Mihály Bányai <banyai.mihaly@wigner.mta.hu>
2016-03-01 21:08:31 +0100

    new type of csm model

---
+++ python/csm2_inference.stan
@@ -0,0 +1,70 @@
+data {
+  int<lower=1> N;
+  int<lower=1> k;
+    
+  int<lower=1> d_x;
+  int<lower=1> d_u;
+
+  real<lower=0> z_shape;
+  real<lower=0> z_scale;
+
+  real<lower=0> g_shape;
+  real<lower=0> g_scale;
+
+  matrix[d_u,d_u] C[k];
+  cov_matrix[d_u] Var_u;
+  matrix[d_u, d_x] A;
+  cov_matrix[d_x] sigma_x;
+
+  vector[d_x] x[N];
+}
+
+transformed data {
+  real z_rate; 
+  real g_rate; 
+  vector[d_u] mu_u;   
+
+  z_rate <- 1 / z_scale;
+  g_rate <- 1 / g_scale;
+
+  for (i in 1:d_u)
+    mu_u[i] <- 0.0;
+}
+
+parameters {
+  vector[k] g[N];
+  real<lower=0.001> z[N];
+  vector[d_u] u[N];
+}
+
+model {
+  matrix[d_u,d_u] C_u;
+  vector[2] actEl;
+  actEl[1] <-  1.0;
+    
+  z ~ gamma(z_shape,z_rate);  
+  
+  for (i in 1:N) {
+    for (j in 1:k)
+      g[i,j] ~ gamma(g_shape,g_rate);
+
+    # add weighed components
+    C_u <- g[i,1] * C[1];
+    for (j in 2:k)
+      C_u <- C_u + g[i,j] * C[j];
+
+    # add unit diagonal and max out at 1
+    for (l in 1:d_u){
+      for (m in 1:d_u){
+        actEl[2] <-  C_u[l,m];
+        C_u[l,m] <- if_else (l==m,  1.0, min(actEl));
+      }
+    }
+
+    # transform with variance
+    C_u <- Var_u * C_u * Var_u;
+      
+    u ~ multi_normal(mu_u,C_u);
+    x[i] ~ multi_normal(z[i]*A*u[i],sigma_x);
+  }
+}
--- python/csm_inference.py
+++ python/csm_inference.py
@@ -1,9 +1,10 @@
 import pickle
-from numpy import zeros, identity, mean
+from numpy import zeros, ones, identity, mean, minimum
 from numpy.random import gamma, dirichlet, multivariate_normal
 from pystan import StanModel
 
 recompile = False
+model_type = 2
 N = 1
 k = 2
 d_x = 16
@@ -14,25 +15,34 @@ z_shape = 2
 g_scale = 0.1
 g_shape = 1
 A = identity(d_x)
+Var_u = identity(d_x)
 templates = [[0, 1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14, 15]]
 C = []
 for i in range(k):
-    act_C = identity(d_u)
+    if model_type == 1:
+        act_C = identity(d_u)
+    elif model_type == 2:
+        act_C = zeros((d_u, d_u))
     for j in range(len(templates[i])):
+        idx1 = templates[i][j]
         for h in range(j+1, len(templates[i])):
-            act_C[j, h] = 0.9
-            act_C[h, j] = act_C[j, h]
+            idx2 = templates[i][h]
+            act_C[idx1, idx2] = 0.9
+            act_C[idx2, idx1] = act_C[idx1, idx2]
     C.append(act_C)
 
 z_synth = gamma(z_shape, z_scale, N)
-# g_synth = gamma(g_shape, g_scale, (N, k))
-g_synth = dirichlet((1, 1), N)
+g_synth = gamma(g_shape, g_scale, (N, k))
+# g_synth = dirichlet((1, 1), N)
 # u_synth = multivariate_normal(zeros(d_u), C, N)
 x_synth = zeros((N, d_x))
 for i in range(N):
     act_C = zeros((d_u, d_u))
     for j in range(k):
         act_C += g_synth[i, j] * C[j]
+    if model_type == 2:
+        act_C = minimum(act_C, ones((d_u, d_u))) + identity(d_u)
+        act_C = Var_u * act_C * Var_u
     act_u = multivariate_normal(zeros(d_u), act_C)
     act_mean = z_synth[i] * A.dot(act_u)
     x_synth[i, :] = multivariate_normal(act_mean, sigma_x)
@@ -49,15 +59,21 @@ gsm_dat = {
     'z_shape': z_shape,
     'z_scale': z_scale,
     'g_shape': g_shape,
-    'g_scale': g_scale
+    'g_scale': g_scale,
+    'Var_u': Var_u
 }
 
+if model_type == 1:
+    fname = "csm"
+elif model_type == 2:
+    fname = "csm2"
+
 if recompile:
-    sm = StanModel(file='csm_inference.stan')
-    with open('csm_inference.pkl', 'wb') as f:
+    sm = StanModel(file=fname + '_inference.stan')
+    with open(fname + 'inference.pkl', 'wb') as f:
         pickle.dump(sm, f)
 else:
-    sm = pickle.load(open('csm_inference.pkl', 'rb'))
+    sm = pickle.load(open(fname + '_inference.pkl', 'rb'))
 
 fit = sm.sampling(data=gsm_dat, iter=400, chains=2)
 estimation = fit.extract(permuted=True)
