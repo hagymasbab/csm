259ce0f2e28e76a4bb16eaed5e4e64fde31cfa52
Mihaly Banyai <banyai.mihaly@wigner.mta.hu>
2016-07-27 18:27:06 +0200

    whitening

--- matlab/imageproc/do_whitening.m
+++ matlab/imageproc/do_whitening.m
@@ -1,8 +1,29 @@
 function xw = do_whitening(x,wnode)
 
-tlen = size(x,1);
+[nImage,imageSize] = size(x);
 
 xw=zeros(size(x));
-for i=1:tlen
-    xw(i,:) = (wnode*x(i,:)')';
+
+filterSize = size(wnode,1);
+
+if mod(imageSize,filterSize) ~= 0
+    error('Image size has to be an integer multiple of the filter size');
+end
+nStep = floor(sqrt(imageSize) / sqrt(filterSize));
+
+for i=1:nImage
+    if filterSize == imageSize
+        xw(i,:) = (wnode*x(i,:)')';
+    else
+        actImg = reshape(x(i,:),sqrt(imageSize),sqrt(imageSize));
+        wImg = zeros(sqrt(imageSize));
+        for sx = 1:nStep
+            for sy = 1:nStep               
+                actPatch = actImg((sx-1)*sqrt(filterSize)+1:sx*sqrt(filterSize),(sy-1)*sqrt(filterSize)+1:sy*sqrt(filterSize));
+                wPatch = reshape(wnode * actPatch(:),sqrt(filterSize),sqrt(filterSize));
+                wImg((sx-1)*sqrt(filterSize)+1:sx*sqrt(filterSize),(sy-1)*sqrt(filterSize)+1:sy*sqrt(filterSize)) = wPatch;
+            end
+        end
+        xw(i,:) = wImg(:)';
+    end
 end
